919fd9f6b3ddc292eff77ca0b324a3a99a9acba0
Gareth Nelson <gareth@garethnelson.com>
2016-04-10 02:44:46 +0100

    Reorganise makefile and begin video module

--- Makefile
+++ Makefile
@@ -1,37 +1,36 @@
-CC=gcc
+CC      = gcc
+CFLAGS  = -g -fPIC
+LDFLAGS =
+LIBS    = -lphysfs
+O       = build
 
-CFLAGS=-g
-LDFLAGS=
-LIBS=-lphysfs
-
-O=build
-
-ENGINE_OBJS = $(O)/engine/vfs_pak.o  $(O)/engine/vfs_init.o
+ENGINE_OBJS       = $(O)/engine/vfs_pak.o  $(O)/engine/vfs_init.o
+LAMBDA_VIDEO_OBJS = $(O)/lambda_video/v_init.o
 
 dist/bin/lambda: $(ENGINE_OBJS) $(O)/engine/l_main.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $(ENGINE_OBJS) $(O)/engine/l_main.o -o $@ $(LIBS)
 
-build/core01/libs/video.so:
-	touch build/core01/libs/video.so
+build/core01/libs/lambda_video.so: $(LAMBDA_VIDEO_OBJS)
+	$(CC) -shared -o $(O)/core01/libs/lambda_video.so $(LAMBDA_VIDEO_OBJS)
 
-dist/data/core01.pak: build/core01/libs/video.so
+dist/data/core01.pak: build/core01/libs/lambda_video.so
 	cd build/core01; zip -r ../../dist/data/core01.pak *
 
-dist/data/core02.pak:
+dist/data/core02.pak: sample/gameinfo.def
+	cp sample/gameinfo.def build/core02/
 	cd build/core02; zip ../../dist/data/core02.pak *
 
 enginebin: dist/bin/lambda
 
 corepaks: dist/data/core01.pak dist/data/core02.pak
 
-tools: dist/bin/lmapcomp dist/bin/lgamecomp
-
-all: enginebin tools corepaks
+all: enginebin corepaks
 
 clean:
 	rm -f dist/bin/*
 	rm -f dist/data/*
 	rm -f build/core01/libs/*
+	rm -f build/core02/gameinfo.def
 	rm -f build/engine/*.o
 
 $(O)/%.o: %.c
---
+++ lambda_video/v_init.c
@@ -0,0 +1,41 @@
+//-----------------------------------------------------------------------------
+//
+// $Id:$
+//
+// Copyright (C) 2016 by Gareth Nelson (gareth@garethnelson.com)
+//
+// This file is part of the Lambda engine.
+//
+// The Lambda engine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Lambda engine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+// $Log:$
+//
+// DESCRIPTION:
+//      Part of the lambda_video module, used to create an OpenGL context
+//
+//-----------------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void __attribute__((constructor)) init_module() {
+     printf("lambda_video/v_init.c:init_module() - module loaded\n");
+}
+
+int video_init() {
+    return 0;
+}
