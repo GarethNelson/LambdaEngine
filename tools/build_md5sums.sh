6e85aed69cca84b9da37986f05863c65adb6994a
Gareth Nelson <gareth@garethnelson.com>
2016-04-20 04:47:34 +0100

    switch to using .dylib on OS X

--- tools/build_md5sums.sh
+++ tools/build_md5sums.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-find . -name '*.so' | xargs -n 1 -I{} sh -c 'md5sum {} >{}.md5'
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='darwin'
+fi
+
+if [ $platform == 'linux' ] || [ $platform == 'freebsd' ]; then
+   find . -name '*.so' | xargs -n 1 -I{} sh -c 'md5sum {} >{}.md5'
+elif [ $platform == 'darwin' ]; then
+   find . -name '*.dylib' | xargs -n 1 -I{} sh -c 'md5sum {} >{}.md5'
+fi
