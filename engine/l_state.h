565af502e84954ec5cf3b099f098ed00710a4607
Gareth Nelson <gareth@garethnelson.com>
2016-04-20 15:04:56 +0100

    add more structs in the FSM

--- engine/l_state.h
+++ engine/l_state.h
@@ -27,19 +27,56 @@
 //-----------------------------------------------------------------------------
 #ifndef __L_STATE_H_
 #define __L_STATE_H_
+#include "uthash.h"
 
 /// this indicates what overall state (i.e what stage) the program is in
 typedef enum {
     STARTUP,
     LOADSCREEN,
+    SPLASH,
     MAINMENU,
     INGAME,
     SHUTDOWN
 } e_lambda_stage;
 
 typedef struct {
+    char  hash_k[40];
+    void* hash_v; // we never free() these unless we're replacing them
+    UT_hash_handle hh;
+} global_vals_hash;
+
+typedef struct {
     e_lambda_stage app_stage;
-    void* stage_vals; // pointer to a struct that contains the actual stage-specific state data
+    void* stage_vals;  // pointer to a struct that contains the actual stage-specific state data
+    global_vals_hash* global_vals; // uthash
 } global_state_t;
+
+// *********************************************
+//  SPLASH data
+// *********************************************
+// TODO - multiple logos?
+typedef struct {
+    float cur_alpha;
+} splash_vals_t;
+
+
+
+// *********************************************
+//  LOADSCREEN data
+typedef struct {
+    e_lambda_stage next_stage; // after loading resources, what stage do we go to next?
+                               // realistically will always be MAINMENU, SPLASH or INGAME
+    char next_map[40];         // if next_stage is INGAME, specify the map
+} loader_vals_t;
+
+// *********************************************
+
+/*
+ Transition between states is done manually:
+ 1 - Save any important data from stage_vals to global_vals and do general cleanup
+ 2 - free() global_state->stage_vals
+ 3 - malloc() a new global_state->stage_vals
+ 4 - access your data by casting global_state->state_vals to the relevant struct
+ */
 
 #endif
