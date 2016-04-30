b109ff7dd68275f7e764a6b594e2918db5d18310
Gareth Nelson <gareth@garethnelson.com>
2016-04-30 21:49:46 +0100

    Add SDL2TTF to cmake

--- lambda_render/r_primitives.c
+++ lambda_render/r_primitives.c
@@ -42,11 +42,11 @@
 #include <GL/glu.h>
 #endif
 
+#include <SDL.h>
+#include <SDL_ttf.h>
+
 #define __IN_RENDER_
 #include <lambda_api.h>
-
-#include <ft2build.h>
-#include FT_FREETYPE_H
 
 GLuint load_texture(char* vfs_filename) {
   IMPORT(vfs_cache_filelen)
@@ -63,6 +63,106 @@ GLuint load_texture(char* vfs_filename) {
   return tex_id;
 
 }
+
+void* load_font(char* vfs_filename) {
+  if(!TTF_WasInit()) {
+     TTF_Init();
+  }
+  IMPORT(vfs_cache_filelen)
+  IMPORT(vfs_cache_read)
+  printf("lambda_render/r_primitives.c:load_font() - Loading %s...",vfs_filename);
+  TTF_Font *font;
+
+  unsigned int f_size = vfs_cache_filelen(vfs_filename);
+  void* f_buf = vfs_cache_read(vfs_filename);
+  font = TTF_OpenFontRW(SDL_RWFromMem(f_buf,f_size),1,32);
+
+  printf("DONE!\n");
+}
+
+// taken from the SDL_ttf glfont.c sample
+static int power_of_two(int input)
+{
+    int value = 1;
+
+    while ( value < input ) {
+        value <<= 1;
+    }
+    return value;
+}
+
+GLuint SDL_GL_LoadTexture(SDL_Surface *surface, GLfloat *texcoord)
+{
+    GLuint texture;
+    int w, h;
+    SDL_Surface *image;
+    SDL_Rect area;
+    Uint8  saved_alpha;
+    SDL_BlendMode saved_mode;
+
+    /* Use the surface width and height expanded to powers of 2 */
+    w = power_of_two(surface->w);
+    h = power_of_two(surface->h);
+    texcoord[0] = 0.0f;         /* Min X */
+    texcoord[1] = 0.0f;         /* Min Y */
+    texcoord[2] = (GLfloat)surface->w / w;  /* Max X */
+    texcoord[3] = (GLfloat)surface->h / h;  /* Max Y */
+
+    image = SDL_CreateRGBSurface(
+            SDL_SWSURFACE,
+            w, h,
+            32,
+#if SDL_BYTEORDER == SDL_LIL_ENDIAN /* OpenGL RGBA masks */
+            0x000000FF,
+            0x0000FF00,
+            0x00FF0000,
+            0xFF000000
+#else
+            0xFF000000,
+            0x00FF0000,
+            0x0000FF00,
+            0x000000FF
+#endif
+               );
+    if ( image == NULL ) {
+        return 0;
+    }
+
+    /* Save the alpha blending attributes */
+    SDL_GetSurfaceAlphaMod(surface, &saved_alpha);
+    SDL_SetSurfaceAlphaMod(surface, 0xFF);
+    SDL_GetSurfaceBlendMode(surface, &saved_mode);
+    SDL_SetSurfaceBlendMode(surface, SDL_BLENDMODE_NONE);
+
+    /* Copy the surface into the GL texture image */
+    area.x = 0;
+    area.y = 0;
+    area.w = surface->w;
+    area.h = surface->h;
+    SDL_BlitSurface(surface, &area, image, &area);
+
+    /* Restore the alpha blending attributes */
+    SDL_SetSurfaceAlphaMod(surface, saved_alpha);
+    SDL_SetSurfaceBlendMode(surface, saved_mode);
+
+    /* Create an OpenGL texture for the image */
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D,
+             0,
+             GL_RGBA,
+             w, h,
+             0,
+             GL_RGBA,
+             GL_UNSIGNED_BYTE,
+             image->pixels);
+    SDL_FreeSurface(image); /* No longer needed */
+
+    return texture;
+}
+
 
 // TODO - switch to VBOs
 
