bd7a82d722fa57697facc2faab568d4f1863fb8a
Gareth Nelson <gareth@garethnelson.com>
2016-05-01 20:07:16 +0100

    Import SDL_image and flesh out lambdamc more

--- tools/lambdamc/lambdamc.c
+++ tools/lambdamc/lambdamc.c
@@ -26,5 +26,83 @@
 //
 //-----------------------------------------------------------------------------
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include "ini.h"
+
+typedef struct {
+    char* map_name;
+    unsigned int tile_res_w;
+    unsigned int tile_res_h;
+} mapinfo_t;
+
+static mapinfo_t mapinfo;
+
+#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+
+static int def_handler(void* user, const char* section, const char* name, const char* value) {
+       if(MATCH("MAPINFO","MAPNAME")) {
+          mapinfo.map_name = strdup(value);
+       } else if (MATCH("MAPINFO","TILERESW")) {
+          mapinfo.tile_res_w = atoi(value);
+       } else if (MATCH("MAPINFO","TILERESH")) {
+          mapinfo.tile_res_h = atoi(value);
+       } else {
+         return 0;
+       }
+       return 1;
+}
+
+void print_usage() {
+     printf("usage: lambdamc -i map_source_dir [-o map_output_dir]\n");
+}
+
 int main(int argc, char** argv) {
+    extern char *optarg;
+    extern int optind;
+
+    char* src_dir =NULL;
+    char* dest_dir=NULL;
+    char mapinfo_def_filename[PATH_MAX];
+    int c = 0;
+
+    if(argc==1) {
+       print_usage();
+       exit(0);
+    }
+    while ((c = getopt(argc, argv, "i:o:")) != -1) {
+       switch(c) {
+          case 'i':
+             src_dir = optarg;
+          break;
+
+          case 'o':
+             dest_dir = optarg;
+          break;
+
+          case '?':
+             print_usage();
+             exit(1);
+          break;
+       }
+    }
+
+    if(src_dir==NULL) {
+        fprintf(stderr, "%s: missing -i option\n",argv[0]);
+        print_usage();
+        exit(1);
+    }
+    if(dest_dir==NULL) {
+        dest_dir = strdup(src_dir);
+    }
+    
+    snprintf(mapinfo_def_filename,PATH_MAX-1,"%s/%s",src_dir,"mapinfo.def");
+    
+    if(ini_parse(mapinfo_def_filename, def_handler, NULL) < 0) {
+       fprintf(stderr,"Failed to load mapinfo.def\n");
+    }
+    
 }
