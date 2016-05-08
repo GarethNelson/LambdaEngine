7ec3e0e0fa3f04539b3dee35574e0fdad24958df
Gareth Nelson <gareth@garethnelson.com>
2016-05-08 07:17:37 +0100

    finish off mapinfo.def loader

--- tools/lambdamc/lambdamc.c
+++ tools/lambdamc/lambdamc.c
@@ -35,7 +35,34 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+#include <uthash.h>
 #include "ini.h"
+
+typedef struct {
+    char* filename;
+    unsigned int hz_count;
+} hotzonemap_t;
+
+typedef struct {
+    char* id;
+    unsigned int r;
+    unsigned int g;
+    unsigned int b;
+    UT_hash_handle hh;
+} hotzone_t;
+
+typedef struct {
+    char* filename;
+    unsigned int e_count;
+} entitymap_t;
+
+typedef struct {
+    char* id;
+    unsigned int r;
+    unsigned int g;
+    unsigned int b;
+    UT_hash_handle hh;
+} entity_t;
 
 typedef struct {
     char* map_name;
@@ -43,19 +70,114 @@ typedef struct {
     unsigned int tile_res_h;
 } mapinfo_t;
 
-static mapinfo_t mapinfo;
+typedef struct {
+    char* filename;
+} mapvis_t;
 
+typedef struct {
+    mapinfo_t     mapinfo;
+    mapvis_t      mapvis;
+    hotzonemap_t  hotzone_map;
+    entitymap_t   entity_map;
+    hotzone_t*    hotzones;
+    entity_t*     entities;
+} mapdef_t;
+
+static mapdef_t mapdef;
+static hotzone_t *hotzone_tmp;
+static entity_t  *entity_tmp;
+
+static char* hotzone_id=NULL; // used for caching which section we're in
+static char* entity_id=NULL;
 #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+
+// bad style i know
+void update_hz_colour(char* hotzone_id, char* colstr) {
+     unsigned int r;
+     unsigned int g;
+     unsigned int b;
+     sscanf(colstr,"%u,%u,%u",&r,&g,&b);
+     HASH_FIND_STR(mapdef.hotzones,hotzone_id,hotzone_tmp);
+     hotzone_tmp->r = r;
+     hotzone_tmp->g = g;
+     hotzone_tmp->b = b;
+}
+
+void update_ent_colour(char* entity_id, char* colstr) {
+     unsigned int r;
+     unsigned int g;
+     unsigned int b;
+     sscanf(colstr,"%u,%u,%u",&r,&g,&b);
+     HASH_FIND_STR(mapdef.entities,entity_id,entity_tmp);
+     entity_tmp->r = r;
+     entity_tmp->g = g;
+     entity_tmp->b = b;
+}
 
 static int def_handler(void* user, const char* section, const char* name, const char* value) {
        if(MATCH("MAPINFO","MAPNAME")) {
-          mapinfo.map_name = strdup(value);
+          mapdef.mapinfo.map_name = strdup(value);
        } else if (MATCH("MAPINFO","TILERESW")) {
-          mapinfo.tile_res_w = atoi(value);
+          mapdef.mapinfo.tile_res_w = atoi(value);
        } else if (MATCH("MAPINFO","TILERESH")) {
-          mapinfo.tile_res_h = atoi(value);
+          mapdef.mapinfo.tile_res_h = atoi(value);
+       } else if (MATCH("MAPVIS","FILENAME")) {
+          mapdef.mapvis.filename = strdup(value);
+       } else if (MATCH("ENTITYMAP","FILENAME")) {
+          mapdef.entity_map.filename = strdup(value);
+       } else if (MATCH("ENTITYMAP","ECOUNT")) {
+          mapdef.entity_map.e_count = atoi(value);
+          mapdef.entities           = NULL;
+       } else if (MATCH("HOTZONEMAP","FILENAME")) {
+          mapdef.hotzone_map.filename = strdup(value);
+       } else if (MATCH("HOTZONEMAP","HZCOUNT")) {
+          mapdef.hotzone_map.hz_count = atoi(value);
+          mapdef.hotzones             = NULL;
+       } else if (MATCH("HOTZONE","ID")){
+          hotzone_id = strdup(value);
+          HASH_FIND_STR(mapdef.hotzones,value,hotzone_tmp);
+          if (hotzone_tmp==NULL) {
+              hotzone_tmp = malloc(sizeof(hotzone_t));
+              hotzone_tmp->id = strdup(value);
+              hotzone_tmp->r  = 0;
+              hotzone_tmp->g  = 0;
+              hotzone_tmp->b  = 0;
+              HASH_ADD_STR(mapdef.hotzones,id,hotzone_tmp);
+          } else {
+              fprintf(stderr,"Duplicate hotzone ID %s\n", value);
+              return 0;
+          }
+       } else if (MATCH("HOTZONE","COLOUR")) {
+          if(hotzone_id==NULL) {
+              fprintf(stderr,"Could not locate hotzone ID\n");
+              return 0;
+          } else {
+              update_hz_colour(hotzone_id,strdup(value));
+          }
+       } else if (MATCH("ENTITY","ID")) {
+          entity_id = strdup(value);
+          HASH_FIND_STR(mapdef.entities,value,entity_tmp);
+          if (entity_tmp==NULL) {
+              entity_tmp = malloc(sizeof(entity_t));
+              entity_tmp->id = strdup(value);
+              entity_tmp->r  = 0;
+              entity_tmp->g  = 0;
+              entity_tmp->b  = 0;
+              HASH_ADD_STR(mapdef.entities,id,entity_tmp);
+          } else {
+              fprintf(stderr,"Duplicate entity ID %s\n", value);
+              return 0;
+          }
+       } else if (MATCH("ENTITY","COLOUR")) {
+          if(entity_id==NULL) {
+              fprintf(stderr,"Could not locate entity ID\n");
+              return 0;
+          } else {
+              update_ent_colour(entity_id,strdup(value));
+          } 
        } else {
-         return 0;
+          fprintf(stderr,"Unknown section/name pair %s:%s\n",section,name);
+          return 0;
        }
        return 1;
 }
@@ -72,6 +194,7 @@ int main(int argc, char** argv) {
     char* dest_dir=NULL;
     char mapinfo_def_filename[PATH_MAX];
     int c = 0;
+    int parse_error = 0;
 
     if(argc==1) {
        print_usage();
@@ -105,8 +228,13 @@ int main(int argc, char** argv) {
     
     snprintf(mapinfo_def_filename,PATH_MAX-1,"%s/%s",src_dir,"mapinfo.def");
     
-    if(ini_parse(mapinfo_def_filename, def_handler, NULL) < 0) {
-       fprintf(stderr,"Failed to load mapinfo.def\n");
+    parse_error = ini_parse(mapinfo_def_filename, def_handler, NULL);
+    if (parse_error != 0) {
+       if (parse_error >0) {
+          fprintf(stderr,"First error on line %d of %s\n",parse_error,mapinfo_def_filename);
+       } else if(parse_error==-1) {
+          fprintf(stderr,"Could not open %s\n",mapinfo_def_filename);
+       }
     }
     
 }
