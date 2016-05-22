dd767a6681e2b3e127201e88657fbe84aeb14d5b
Gareth Nelson <gareth@garethnelson.com>
2016-05-22 06:39:20 +0100

    some more hacking on lambdaextractcol tool

--- tools/lambdaextractcol/lambdaextractcol.c
+++ tools/lambdaextractcol/lambdaextractcol.c
@@ -32,6 +32,7 @@
 #include <string.h>
 #include <limits.h>
 #include <errno.h>
+#include <def_format.h>
 
 #include <wand/magick_wand.h>
 
@@ -107,7 +108,91 @@ int main(int argc, char** argv) {
        exit(1);
     }
 
+    if(verbose) {
+       fprintf(stderr,"Loading definitions\n");
+    }
+
+    def_database_t* visual_defs = read_def_fd(input_fd);
+    def_record_t*   tiles       = def_getall(visual_defs,"TILE");
+    if(verbose) {
+       fprintf(stderr,"Loaded definitions for %d tiles\n",tiles->type_count);
+    }
+
+    if(verbose) {
+       fprintf(stderr,"Loading textures for analysis\n");
+    }
+
+    setbuf(stdout,NULL);
+    setbuf(stderr,NULL);
     MagickWandGenesis();
+    unsigned int i=0;
+    char* texture_val;
+    char texture_id[6];
+    char texture_path[PATH_MAX];
+    MagickWand **wands = (MagickWand**)malloc(sizeof(MagickWand*)*tiles->type_count);
+    MagickBooleanType status;
+    unsigned int f=0;
+    for(i=0; i< tiles->type_count; i++) {
+        // TODO - look into caching here (the engine does it fine using the VFS cache, tools should too)
+        wands[i]    = NewMagickWand();
+        for(f=0; f< tiles[i].field_count; f++) {
+            if(strcmp(tiles[i].fields[f].field_name,"TEXTURE")==0) {
+               texture_val = tiles[i].fields[f].field_val;
+            }
+        }
+        snprintf(texture_path,PATH_MAX-1,"%s/%s",output_path,texture_val);
+        fprintf(stderr,"Trying to load %s now...",texture_path);
+        status      = MagickReadImage(wands[i],texture_path);
+        if(status) {
+           if(verbose) {
+              fprintf(stderr,"DONE!\n");
+           }
+        } else {
+           if(verbose) {
+              fprintf(stderr,"FAIL!\n");
+           } else {
+              fprintf(stderr,"Failed to load %s\n",texture_path);
+           }
+           exit(1);
+        }
+    }
+
+    if(verbose) {
+       fprintf(stderr,"Loaded textures, starting analysis\n");
+    }
+
+    // TODO - make this something other than just stdout
+    unsigned int tile_x=0;
+    unsigned int tile_y=0;
+    unsigned int tile_w=0;
+    unsigned int tile_h=0;
+    double pixel_r=0;
+    double pixel_g=0;
+    double pixel_b=0;
+    PixelWand* color_wand;
+    color_wand = NewPixelWand();
+    for(i=0; i< tiles->type_count; i++) {
+        tile_w = (unsigned int)MagickGetImageWidth(wands[i]);
+        tile_h = (unsigned int)MagickGetImageHeight(wands[i]);
+        for(tile_x=0; tile_x<tile_w; tile_x++) {
+            for(tile_y=0; tile_y<tile_h; tile_y++) {
+                MagickGetImagePixelColor(wands[i],tile_x,tile_y,color_wand);
+                pixel_r = PixelGetRed(color_wand);
+                pixel_g = PixelGetGreen(color_wand);
+                pixel_b = PixelGetBlue(color_wand);
+                printf("%f %f %f\n",pixel_r,pixel_g,pixel_b);
+                if((pixel_r==red_c) && (pixel_g==green_c) && (pixel_b==blue_c)) {
+                  printf("TILE\n");
+                  printf("ID: %d\n",i);
+                  printf("%%%%\n");
+                  break;
+                }
+            }
+            if((pixel_r==red_c) && (pixel_g==green_c) && (pixel_b==blue_c)) {
+               break;
+            }
+        }
+    }
 
     MagickWandTerminus();
 
